54668f96d3b57ccfb74a3400759a25b52e5f193e
Devon Beets <dbeetzz@gmail.com>
2017-04-22 15:21:34 -0600

    added kitties pic and updating templates and css! :smiley_cat:

--- public/templates/home.php
+++ public/templates/home.php
@@ -1,19 +1,27 @@
-<div align="center">
-	<h2 class="new-twitter">Start Searching Now For New Twitter Ipsum!</h2>
-	<div class="row">
-		<div class="col-xs-8 col-xs-offset-2">
-			<div class="input-group main-search">
-				<input type="text" class="form-control" placeholder="@realDonaldTrump">
-				<span class="input-group-btn">
+<div class="container container-fluid">
+	<div align="center">
+		<h2>Start Searching Now For New Twitter Ipsum!</h2>
+		<div class="row">
+			<div class="col-xs-8 col-xs-offset-2">
+				<div class="input-group main-search">
+					<input type="text" class="form-control" placeholder="@realDonaldTrump">
+					<span class="input-group-btn">
         			<button class="btn btn-default" type="button">Generate!</button>
       		</span>
+				</div>
 			</div>
 		</div>
 		<div class="row">
 			<div class="col-xs-8 col-xs-offset-2">
 				<div align="center">
 					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit!</p><br/>
-					<p>Twitter ipsum is a project made for Angular Attack 2017 that generates lorem ipsum, or text that is used as filler in graphic and web design, from Tweets sent by anyone you like! Just enter a search for their Twitter handle, select an amount of text to make, and click generate! Our Angular boosted app will take care of the rest!</p><br />
+					<p>Twitter ipsum is a project made for Angular Attack 2017 that generates lorem ipsum, or text that is
+						used
+						as filler in graphic and web design, from Tweets sent by anyone you like! Just enter a search for
+						their
+						Twitter handle, select an amount of text to make, and click generate! Our Angular boosted app will
+						take
+						care of the rest!</p><br/>
 					<p>Have fun!</p>
 					<div class="btn-group btn-group-justified" role="group" aria-label="Check out other Ipsum!">
 						<div class="btn-group" role="group">
@@ -33,7 +41,15 @@
 					<br/>
 					<br/>
 					<br/>
-						<p class="ipsum-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus non justo at mauris tristique viverra sit amet et enim. Nulla ac scelerisque nisl. Pellentesque sagittis euismod ullamcorper. Praesent ullamcorper tristique aliquam. Nullam semper sed urna et lobortis. Aliquam sed eros lorem. Vestibulum sodales lectus vel quam molestie, ac tempor ante vehicula. Quisque elementum est turpis, vel fringilla ipsum vehicula non. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla facilisi.</p>
+					<p class="ipsum-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus non justo at
+						mauris
+						tristique viverra sit amet et enim. Nulla ac scelerisque nisl. Pellentesque sagittis euismod
+						ullamcorper.
+						Praesent ullamcorper tristique aliquam. Nullam semper sed urna et lobortis. Aliquam sed eros lorem.
+						Vestibulum sodales lectus vel quam molestie, ac tempor ante vehicula. Quisque elementum est turpis,
+						vel
+						fringilla ipsum vehicula non. Orci varius natoque penatibus et magnis dis parturient montes, nascetur
+						ridiculus mus. Nulla facilisi.</p>
 				</div>
 			</div>
 		</div>
--- public/templates/profile.php
+++ public/templates/profile.php
@@ -1,8 +1,9 @@
 <div align="center">
 	<div class="row">
-		<div class="col-xs-6 col-xs-offset-6">
-			<div class="well">
-				<h1>Hello World!</h1>
+		<div class="col-xs-4 col-xs-offset-4">
+			<div class="well well-lg">
+				<h3>@profileAtHandle</h3>
+				<h3>profileEmail</h3>
 			</div>
 		</div>
 	</div>
@@ -17,12 +18,19 @@
 			<br/>
 			<br/>
 			<br/>
-			<p class="ipsum-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus non justo at mauris tristique viverra sit amet et enim. Nulla ac scelerisque nisl. Pellentesque sagittis euismod ullamcorper. Praesent ullamcorper tristique aliquam. Nullam semper sed urna et lobortis. Aliquam sed eros lorem. Vestibulum sodales lectus vel quam molestie, ac tempor ante vehicula. Quisque elementum est turpis, vel fringilla ipsum vehicula non. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla facilisi. <button type="button" class="btn btn-default copy-button pull-right">Share +</button></p>
+			<p class="ipsum-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus non justo at mauris
+				tristique viverra sit amet et enim. Nulla ac scelerisque nisl. Pellentesque sagittis euismod ullamcorper.
+				Praesent ullamcorper tristique aliquam. Nullam semper sed urna et lobortis. Aliquam sed eros lorem.
+				Vestibulum sodales lectus vel quam molestie, ac tempor ante vehicula. Quisque elementum est turpis, vel
+				fringilla ipsum vehicula non. Orci varius natoque penatibus et magnis dis parturient montes, nascetur
+				ridiculus mus. Nulla facilisi.
+				<button type="button" class="btn btn-default copy-button pull-right">Share</button>
+			</p>
 		</div>
 	</div>
 </div>
 <div class="row">
-	<div class="col-xs-8 ol-xs-offset-2">
+	<div class="col-xs-8 col-xs-offset-2">
 		<div class="well well-lg ipsum-well">
 			<div class="at-handle pull-left">
 				<h4>@twitterUserAtHandle</h4>
@@ -31,9 +39,14 @@
 			<br/>
 			<br/>
 			<br/>
-			<p class="ipsum-text">>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus non justo at mauris tristique viverra sit amet et enim. Nulla ac scelerisque nisl. Pellentesque sagittis euismod ullamcorper. Praesent ullamcorper tristique aliquam. Nullam semper sed urna et lobortis. Aliquam sed eros lorem. Vestibulum sodales lectus vel quam molestie, ac tempor ante vehicula. Quisque elementum est turpis, vel fringilla ipsum vehicula non. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla facilisi. <button type="button" class="btn btn-default copy-button pull-right">Share +</button></p>
-		</div>
-		</div>
+			<p class="ipsum-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus non justo at mauris
+				tristique viverra sit amet et enim. Nulla ac scelerisque nisl. Pellentesque sagittis euismod ullamcorper.
+				Praesent ullamcorper tristique aliquam. Nullam semper sed urna et lobortis. Aliquam sed eros lorem.
+				Vestibulum sodales lectus vel quam molestie, ac tempor ante vehicula. Quisque elementum est turpis, vel
+				fringilla ipsum vehicula non. Orci varius natoque penatibus et magnis dis parturient montes, nascetur
+				ridiculus mus. Nulla facilisi.
+				<button type="button" class="btn btn-default copy-button pull-right">Share +</button>
+			</p>
 		</div>
 	</div>
 </div>

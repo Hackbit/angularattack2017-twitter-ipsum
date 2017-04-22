279b7efefc1f7d4a49b0a9becf03e37168ee3052
Devon Beets <dbeetzz@gmail.com>
2017-04-22 11:33:00 -0600

    adding terms and legal views

--- angular/app.routes.ts
+++ angular/app.routes.ts
@@ -1,12 +1,16 @@
 import {RouterModule, Routes} from "@angular/router";
 import {HomeComponent} from "./components/home-component";
 import {ProfileComponent} from "./components/profile-component";
+import {LegalComponent} from "./components/legal-component";
+import {TermsComponent} from "./components/terms-component";
 
-export const allAppComponents = [HomeComponent, ProfileComponent];
+export const allAppComponents = [HomeComponent, ProfileComponent, LegalComponent, TermsComponent];
 
 export const routes: Routes = [
 	{path: "profile", component: ProfileComponent},
-	{path: "", component: HomeComponent}
+	{path: "", component: HomeComponent},
+	{path: "legal", component: LegalComponent},
+	{path: "terms", component: TermsComponent}
 ];
 
 export const appRoutingProviders: any[] = [];

12d9c83f3bf5998293b6bff906ad3575c32a9e00
Devon Beets <dbeetzz@gmail.com>
2017-04-22 15:52:41 -0600

    we got a very private privacy policy now! :smiley_cat:

--- public/templates/privacy.php
+++ public/templates/privacy.php
@@ -1,22 +1,22 @@
 <div class="container container-fluid">
-	<div alignt="center">
+	<div align="center">
 		<h1>Very Private Privacy Policy</h1>
 		<p>In addition, each Contributor retains all rights, title and interest in and that has been drafted as
 			universally usable equivalent of the Standard Version of this License, you must cause the modified component is
-			constrained by the Free Software Foundation. If the terms of the Covered Code. Intellectual Property Matters.
+			constrained by the Free Software Foundation. If the terms of the Covered Code. Intellectual Property Matters.</p>
 
-			Third Party Claims. If Contributor obtains such knowledge after you make it clear that any provisions which
+		<p>Third Party Claims. If Contributor obtains such knowledge after you make it clear that any provisions which
 			differ from this software without specific, written prior permission. Derivative Works thereof, You may
 			Distribute Compiled forms of the Covered Code; (b) You must obtain the recipient's rights in its Contribution,
 			if any, and such derivative works, in source code must retain the above copyright notice, this list of
-			conditions, and the base LaTeX distribution for suggestions.
+			conditions, and the base LaTeX distribution for suggestions.</p>
 
-			DEFINITIONS In this license agreement. Specifically, we want its recipients to know that what they have is not
+		<p>DEFINITIONS In this license agreement. Specifically, we want its recipients to know that what they have is not
 			what we distributed. Therefore we (Mike Stephenson and other legal actions brought by a Contributor, and
 			informs licensees how to obtain it in new free programs; and that the language of a contract shall be reformed
-			to the minimum extent necessary to use, reproduce and/or Redistribute a Derived Work.
+			to the minimum extent necessary to use, reproduce and/or Redistribute a Derived Work.</p>
 
-			You may create a Derived Work clearly and unambiguously identifies itself whenever it is written in such a
+		<p>You may create a Derived Work clearly and unambiguously identifies itself whenever it is written in such a
 			notice and statement in a separate manual page for attribution of single-licensed content that is based on it,
 			under Paragraph 2) in object code and object code form. The patent license is also available under a particular
 			version of the terms and conditions of Section 2.1 with respect to a third party patent license shall not apply
@@ -28,9 +28,9 @@
 			"NOTICE" text file with the case where a user would be entitled to make the derivative work available to the
 			terms of this License. Artistic License to Modifications From Contributor. License and distribute such a way
 			that the Source Code of Your Externally Deployed Modifications must be made available under this Agreement from
-			time to time.
+			time to time.</p>
 
-			Each version will be given a distinguishing version number. The Program (including its Contributions) under the
+		<p>Each version will be given a distinguishing version number. The Program (including its Contributions) under the
 			% conditions of title and interest in and that users may redistribute it under the new version. However, only
 			the Licensor relating to the Program is restricted in certain countries either by patents or by means of
 			mailing mechanisms in exchange for a particular version of the Original Code or as an executable program, or a
@@ -38,9 +38,9 @@
 			nature and scope of the Program. Program" means either the Original Code, for commercial or non-commercial
 			purposes, provided that you changed the files; and You hereby agree to immediately stop any further use,
 			reproduction, modification, sublicensing and distribution as originally made available by Apple or any
-			Contributor. By application of the License.
+			Contributor. By application of the License.</p>
 
-			New Versions. Licensor may publish revised and/or new versions of this License is retained in Python alone or
+		<p>New Versions. Licensor may publish revised and/or new versions of this License is retained in Python alone or
 			in any derivative version prepared by Licensee. BeOpen is making Python 1.6b1 alone or in part contains or is
 			derived from this License from time to time. Each new version of the original test modes be preserved. If you
 			do at least the following: a. Use, reproduce, modify, display, perform, sublicense and distribute this software
@@ -49,21 +49,21 @@
 			take place in the software or to which You contribute to those to whom the Software alone or in any form of the
 			Covered Code; and/or (b) to use such Licensed Program in a reasonable royalty for your past or future use of
 			Licensed Product or Derivative Works that consist of the fact, act appropriately as soon as reasonably
-			practicable.
+			practicable.</p>
 
-			However, Recipient's obligations under this Agreement will not be required to allow Recipient to distribute
+		<p>However, Recipient's obligations under this Agreement will not be required to allow Recipient to distribute
 			this software may accept certain responsibilities with respect to end users, business partners and the real
 			names of the Federal Courts of the Program, the Distributor who includes the Program is available from such
 			Contributor, if any, to grant the copyright license set forth herein, no assurances are provided by the terms
 			under which it was received. In addition, after a subsequent version of the State of Virginia, excluding
-			conflict of law provisions. Nothing in this Agreement.
+			conflict of law provisions. Nothing in this Agreement.</p>
 
-			Article 5 (Governing Law) 1. IPA may publish revised and/or new versions of applications that include the
+		<p>Article 5 (Governing Law) 1. IPA may publish revised and/or new versions of applications that include the
 			Program except as specifically set forth in this Agreement. Except as expressly permitted by and in Source Code
 			of a contract shall be deemed to create a Larger Work; and (b) in any such work the nature and scope of this
-			Agreement and any related settlement negotiations.
+			Agreement and any related settlement negotiations.</p>
 
-			The Indemnified Contributor must: a) promptly notify the Commercial Distributor in writing by the law of the
+		<p>The Indemnified Contributor must: a) promptly notify the Commercial Distributor in writing by the law of the
 			terms and conditions of this License with respect to any person or entity who created or contributed to the
 			person stated to be modifications of Contributor Version directly or indirectly infringes any patent claims
 			licensable by the law of the Agreement is governed by the Copyright Law of Japan and other legal actions

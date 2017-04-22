d7e16a0c57a3f657f3c12121aae091d429a4a88a
S JACKSON <jackson.sabastian@gmail.com>
2017-04-22 14:18:12 -0600

    second well completion coming soon :-) - discverd issue with the first copy and share

--- public/templates/profile.php
+++ public/templates/profile.php
@@ -21,3 +21,19 @@
 		</div>
 	</div>
 </div>
+<div class="row">
+	<div class="col-xs-8 ol-xs-offset-2">
+		<div class="well well-lg ipsum-well">
+			<div class="at-handle pull-left">
+				<h4>@twitterUserAtHandle</h4>
+			</div>
+			<button type="button" class="btn btn-default copy-button pull-right">Copy +</button>
+			<br/>
+			<br/>
+			<br/>
+			<p class="ipsum-text">>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus non justo at mauris tristique viverra sit amet et enim. Nulla ac scelerisque nisl. Pellentesque sagittis euismod ullamcorper. Praesent ullamcorper tristique aliquam. Nullam semper sed urna et lobortis. Aliquam sed eros lorem. Vestibulum sodales lectus vel quam molestie, ac tempor ante vehicula. Quisque elementum est turpis, vel fringilla ipsum vehicula non. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla facilisi. <button type="button" class="btn btn-default copy-button pull-right">Share +</button></p>
+		</div>
+		</div>
+		</div>
+	</div>
+</div>

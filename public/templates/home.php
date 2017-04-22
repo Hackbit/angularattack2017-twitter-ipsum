6e8cbde2087fed34c4d2527cec59dbfb15199d3d
Devon Beets <dbeetzz@gmail.com>
2017-04-22 09:57:56 -0600

    updating css and splash page structure

--- public/templates/home.php
+++ public/templates/home.php
@@ -1,8 +1,8 @@
 <div align="center">
 	<h2>Start Searching Now For New Twitter Ipsum!</h2>
 	<div class="row">
-		<div class="col-xs-6 col-xs-offset-3">
-			<div class="input-group input-group-lg main-search">
+		<div class="col-xs-8 col-xs-offset-2">
+			<div class="input-group main-search">
 				<input type="text" class="form-control" placeholder="@realDonaldTrump">
 				<span class="input-group-btn">
         			<button class="btn btn-default" type="button">Generate!</button>
@@ -11,17 +11,29 @@
 		</div>
 		<div class="row">
 			<div class="col-xs-8 col-xs-offset-2">
-				<div class="well">
-					<div align="center">
-						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit!</p><br/>
-						<p>Twitter ipsum is a project made for Angular Attack 2017 that generates lorem ipsum, or text that is used as filler in graphic and web design, from Tweets sent by anyone you like! Just enter a search for their Twitter handle, select an amount of text to make, and click generate! Our Angular boosted app will take care of the rest!</p><br />
-						<p>Have fun!</p>
-						<div class="btn-group btn-group-justified" role="group" aria-label="Check out other Ipsum!">
-							<div class="btn-group" role="group">
-								<button type="button" class="btn btn-default">See What Ipsum Others Made!</button>
-							</div>
+				<div align="center">
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit!</p><br/>
+					<p>Twitter ipsum is a project made for Angular Attack 2017 that generates lorem ipsum, or text that is used as filler in graphic and web design, from Tweets sent by anyone you like! Just enter a search for their Twitter handle, select an amount of text to make, and click generate! Our Angular boosted app will take care of the rest!</p><br />
+					<p>Have fun!</p>
+					<div class="btn-group btn-group-justified" role="group" aria-label="Check out other Ipsum!">
+						<div class="btn-group" role="group">
+							<button type="button" class="btn btn-default">See What Ipsum Others Made!</button>
 						</div>
 					</div>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-xs-8 col-xs-offset-2">
+				<div class="well well-lg ipsum-well">
+					<div class="at-handle pull-left">
+						<h4>@realDonaldTrump</h4>
+					</div>
+					<button type="button" class="btn btn-default copy-button pull-right">Copy +</button>
+					<br/>
+					<br/>
+					<br/>
+						<p class="ipsum-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus non justo at mauris tristique viverra sit amet et enim. Nulla ac scelerisque nisl. Pellentesque sagittis euismod ullamcorper. Praesent ullamcorper tristique aliquam. Nullam semper sed urna et lobortis. Aliquam sed eros lorem. Vestibulum sodales lectus vel quam molestie, ac tempor ante vehicula. Quisque elementum est turpis, vel fringilla ipsum vehicula non. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla facilisi.</p>
 				</div>
 			</div>
 		</div>

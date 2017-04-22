02553bf04783c36b43c0c51df344aed61e4b1584
Devon Beets <dbeetzz@gmail.com>
2017-04-22 07:44:15 -0600

    getting Sebastian started with the profile view!

--- public/templates/profile.php
+++ public/templates/profile.php
@@ -1,1 +1,9 @@
-<h1>Profile Component</h1>
+<div align="center">
+	<div class="row">
+		<div class="col-xs-4 col-xs-offset-4">
+			<div class="well">
+				<h1>Hello World!</h1>
+			</div>
+		</div>
+	</div>
+</div>

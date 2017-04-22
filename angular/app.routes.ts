e18d10f91474af23bf829d05f775ea36c4ea7ba3
Dylan McDonald <dmcdonald21@cnm.edu>
2017-04-21 21:55:41 -0600

    routed profile component

--- angular/app.routes.ts
+++ angular/app.routes.ts
@@ -1,9 +1,11 @@
 import {RouterModule, Routes} from "@angular/router";
 import {HomeComponent} from "./components/home-component";
+import {ProfileComponent} from "./components/profile-component";
 
-export const allAppComponents = [HomeComponent];
+export const allAppComponents = [HomeComponent, ProfileComponent];
 
 export const routes: Routes = [
+	{path: "profile", component: ProfileComponent},
 	{path: "", component: HomeComponent}
 ];
 

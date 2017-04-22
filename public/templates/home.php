d8f24ad0d39d1fe14a1fb348f105d22babc01fb9
Devon Beets <dbeetzz@gmail.com>
2017-04-22 08:54:27 -0600

    html wireframe and whitespace styling

--- public/templates/home.php
+++ public/templates/home.php
@@ -1,3 +1,29 @@
 <div align="center">
-<h1>Start Searching Now For New Twitter Ipsum!</h1>
+	<h2>Start Searching Now For New Twitter Ipsum!</h2>
+	<div class="row">
+		<div class="col-xs-6 col-xs-offset-3">
+			<div class="input-group input-group-lg main-search">
+				<input type="text" class="form-control" placeholder="@realDonaldTrump">
+				<span class="input-group-btn">
+        			<button class="btn btn-default" type="button">Generate!</button>
+      		</span>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-xs-8 col-xs-offset-2">
+				<div class="well">
+					<div align="center">
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit!</p><br/>
+						<p>Twitter ipsum is a project made for Angular Attack 2017 that generates lorem ipsum, or text that is used as filler in graphic and web design, from Tweets sent by anyone you like! Just enter a search for their Twitter handle, select an amount of text to make, and click generate! Our Angular boosted app will take care of the rest!</p><br />
+						<p>Have fun!</p>
+						<div class="btn-group btn-group-justified" role="group" aria-label="Check out other Ipsum!">
+							<div class="btn-group" role="group">
+								<button type="button" class="btn btn-default">See What Ipsum Others Made!</button>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>

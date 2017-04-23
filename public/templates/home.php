cab67ec4dc34c6213513cad6afb7117e3d72e41d
Devon Beets <dbeetzz@gmail.com>
2017-04-23 13:03:14 -0600

    fixed up home view :smiley_cat:

--- public/templates/home.php
+++ public/templates/home.php
@@ -3,17 +3,6 @@
 		<h2 class="white-text">Start Searching Now For New Twitter Ipsum!</h2>
 		<div class="row">
 			<div class="col-xs-8 col-xs-offset-2">
-				<div class="input-group main-search">
-					<span class="input-group-addon" id="at-add-on">@</span>
-					<input type="text" class="form-control" placeholder="realDonaldTrump">
-					<span class="input-group-btn">
-        			<button class="btn btn-default" type="button">Generate!</button>
-      		</span>
-				</div>
-			</div>
-		</div>
-		<div class="row">
-			<div class="col-xs-8 col-xs-offset-2">
 				<div align="center">
 					<p class="white-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit!</p><br/>
 					<p class="white-text">Twitter ipsum is a project made for Angular Attack 2017 that generates lorem ipsum,
@@ -27,34 +16,9 @@
 					<p class="white-text">Have fun!</p>
 					<div class="btn-group btn-group-justified" role="group" aria-label="Check out other Ipsum!">
 						<div class="btn-group" role="group">
-							<button type="button" class="btn btn-default">See What Ipsum Others Made!</button>
+							<a routerLink="feed" type="button" class="btn btn-default">See What Ipsum Others Made!</a>
 						</div>
 					</div>
-				</div>
-			</div>
-		</div>
-		<div class="row">
-			<div class="col-xs-8 col-xs-offset-2">
-				<div class="well well-lg ipsum-well">
-					<div class="pull-left">
-						<img src="../images/kitties49x49.png" alt="KITTIES!">
-					</div>
-					<div class="at-handle pull-left">
-						<p> @realDonaldTrump</p>
-					</div>
-					<button type="button" class="btn btn-default copy-button pull-right">Copy +</button>
-					<br/>
-					<br/>
-					<br/>
-					<p class="ipsum-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus non justo at
-						mauris
-						tristique viverra sit amet et enim. Nulla ac scelerisque nisl. Pellentesque sagittis euismod
-						ullamcorper.
-						Praesent ullamcorper tristique aliquam. Nullam semper sed urna et lobortis. Aliquam sed eros lorem.
-						Vestibulum sodales lectus vel quam molestie, ac tempor ante vehicula. Quisque elementum est turpis,
-						vel
-						fringilla ipsum vehicula non. Orci varius natoque penatibus et magnis dis parturient montes, nascetur
-						ridiculus mus. Nulla facilisi.</p>
 				</div>
 			</div>
 		</div>

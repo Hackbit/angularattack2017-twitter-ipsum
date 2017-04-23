5b7da10fd337d9ec33ea5ecdf55155338a35699a
Dylan McDonald <dmcdonald21@cnm.edu>
2017-04-23 12:45:27 -0600

    added conditional profile page

--- public/templates/profile.php
+++ public/templates/profile.php
@@ -1,3 +1,14 @@
+<?php
+if(session_status() !== PHP_SESSION_ACTIVE) {
+	session_start();
+}
+
+if(empty($_SESSION["profile"]) === true) {
+?>
+<h1>Please Login</h1>
+<p>To generate Twitter Ipsum, please <a href="/twitter/oauth"><i class="fa fa-twitter" aria-hidden="true"></i> Connect With Twitter</a></p>
+<?php } else { ?>
 <h1>Your Ipsums</h1>
 <ipsum-create (ipsumCreated)="ipsumUpdate($event)"></ipsum-create>
 <ipsum *ngFor="let ipsum of ipsums" [ipsum]="ipsum.ipsum" [twitterUser]="ipsum.twitterUser"></ipsum>
+<?php } ?>

75881c9a40470e35b83efb2cccdb772094835d6c
Dylan McDonald <dmcdonald21@cnm.edu>
2017-04-23 00:38:33 -0600

    completed structural directive for ipsum feed

--- angular/components/ipsum-component.ts
+++ angular/components/ipsum-component.ts
@@ -1,8 +1,13 @@
-import {Component} from "@angular/core";
+import {Component, Input} from "@angular/core";
+import {Ipsum} from "../classes/ipsum";
+import {TwitterUser} from "../classes/twitter-user";
 
 @Component({
 	selector: "ipsum",
 	templateUrl: "./templates/ipsum.php"
 })
 
-export class IpsumComponent {}
+export class IpsumComponent {
+	@Input() ipsum : Ipsum;
+	@Input() twitterUser : TwitterUser;
+}
--- public/templates/ipsum.php
+++ public/templates/ipsum.php
@@ -2,22 +2,17 @@
 	<div class="col-xs-8 col-xs-offset-2">
 		<div class="well well-lg ipsum-well">
 			<div class="pull-left">
-				<img src="../images/kitties49x49.png" alt="KITTIES!">
+				<img [src]="twitterUser.twitterUserImage" alt="">
 			</div>
-			<div class="at-handle pull-left">
-				<p> @realDonaldTrump</p>
-			</div>
-			<button type="button" class="btn btn-default copy-button pull-right">Copy +</button>
-			<br/>
-			<br/>
-			<br/>
-			<p class="ipsum-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus non justo at mauris
-				tristique viverra sit amet et enim. Nulla ac scelerisque nisl. Pellentesque sagittis euismod ullamcorper.
-				Praesent ullamcorper tristique aliquam. Nullam semper sed urna et lobortis. Aliquam sed eros lorem.
-				Vestibulum sodales lectus vel quam molestie, ac tempor ante vehicula. Quisque elementum est turpis, vel
-				fringilla ipsum vehicula non. Orci varius natoque penatibus et magnis dis parturient montes, nascetur
-				ridiculus mus. Nulla facilisi.
-				<button type="button" class="btn btn-default copy-button pull-right">Share +</button>
+			<p class="at-handle">
+				@{{ twitterUser.twitterUserAtHandle }}
+			</p>
+			<p class="ipsum-text">
+				{{ ipsum.ipsumContent }}
+			</p>
+			<p>
+				<button type="button" class="btn btn-default"><i class="fa fa-clipboard" aria-hidden="true"></i> Copy</button>
+				<button type="button" class="btn btn-default"><i class="fa fa-twitter" aria-hidden="true"></i> Share</button>
 			</p>
 		</div>
 	</div>

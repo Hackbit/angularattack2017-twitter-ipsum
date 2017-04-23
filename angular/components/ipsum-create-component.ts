98843dd105c47e12d2dfba222724081593175355
Dylan McDonald <dmcdonald21@cnm.edu>
2017-04-23 08:30:55 -0600

    first version of ipsum create form

--- angular/components/ipsum-create-component.ts
+++ angular/components/ipsum-create-component.ts
@@ -1,6 +1,7 @@
-import {Component, Input} from "@angular/core";
-import {Ipsum} from "../classes/ipsum";
-import {TwitterUser} from "../classes/twitter-user";
+import {Component} from "@angular/core";
+import {IpsumRequest} from "../classes/ipsum-request";
+import {IpsumService} from "../services/ipsum-service";
+import {Status} from "../classes/status";
 
 @Component({
 	selector: "ipsum-create",
@@ -8,4 +9,12 @@ import {TwitterUser} from "../classes/twitter-user";
 })
 
 export class IpsumCreateComponent {
+	newIpsumRequest : IpsumRequest = new IpsumRequest(null);
+	status : Status = null;
+
+	constructor(private ipsumService: IpsumService) {}
+
+	createIpsum() : void {
+		this.ipsumService.createIpsum(this.newIpsumRequest).subscribe(status => this.status = status);
+	}
 }
--- public/templates/ipsum-create.php
+++ public/templates/ipsum-create.php
@@ -1,12 +1,22 @@
 <h2>Ipsum Create</h2>
-<div class="row">
-	<div class="col-xs-8 col-xs-offset-2">
-		<div class="input-group main-search">
-			<span class="input-group-addon" id="at-add-on">@</span>
-			<input type="text" class="form-control" placeholder="realDonaldTrump">
-			<span class="input-group-btn">
-				<button class="btn btn-default" type="button">Generate!</button>
+<form #ipsumCreateForm="ngForm" name="ipsumCreateForm" (ngSubmit)="createIpsum();" novalidate>
+	<div class="row">
+		<div class="col-xs-8 col-xs-offset-2">
+			<div class="input-group main-search">
+				<span class="input-group-addon" id="at-add-on">@</span>
+				<input type="text" class="form-control" placeholder="realDonaldTrump" required maxlength="32"
+						 [(ngModel)]="newIpsumRequest.twitterUserAtHandle" #twitterUserAtHandle="ngModel"/>
+				<span class="input-group-btn">
+				<button [disabled]="ipsumCreateForm.invalid" class="btn btn-default" type="submit">Generate!</button>
 			</span>
+			</div>
 		</div>
 	</div>
+	<div [hidden]="twitterUserAtHandle.valid || twitterUserAtHandle.pristine" class="alert alert-danger row" role="alert">
+		<p *ngIf="twitterUserAtHandle.errors?.required">At Handle is required.</p>
+	</div>
+</form>
+<div *ngIf="status !== null" class="alert alert-dismissible row" [ngClass]="status.type" role="alert">
+	<button type="button" class="close" aria-label="Close" (click)="status = null;"><span aria-hidden="true">&times;</span></button>
+	{{ status.message }}
 </div>

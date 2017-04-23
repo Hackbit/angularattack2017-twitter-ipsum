46a343b490bbe95f49a2477545d0e06339a10729
Bradley Brown <tall.white.ninja@gmail.com>
2017-04-22 21:13:28 -0600

    updating ipsum service

--- angular/services/ipsum-service.ts
+++ angular/services/ipsum-service.ts
@@ -3,6 +3,7 @@ import {Http} from "@angular/http";
 import {BaseService} from "./base-service";
 import {Status} from "../classes/status";
 import {Ipsum} from "../classes/ipsum";
+import {IpsumRequest} from "../classes/ipsum-request";
 import {Observable} from "rxjs/Observable";
 
 @Injectable ()
@@ -14,7 +15,22 @@ export class IpsumService extends BaseService {
 	//define the API endpoint
 	private ipsumUrl = "api/ipsum/";
 
-	getFeed() : Observable<Ipsum[]> {
+	createIpsum(ipsumRequest : IpsumRequest) : Observable<Status> {
+		return(this.http.post(this.ipsumUrl, ipsumRequest)
+			.map(BaseService.extractMessage)
+			.catch(BaseService.handleError));
+	}
+	getIpsumByAtHandle(atHandle : string) : Observable<Ipsum[]> {
+		return(this.http.get(this.ipsumUrl + "at-handle/" + atHandle)
+			.map(BaseService.extractData)
+			.catch(BaseService.handleError));
+	}
+	getIpsumByProfileId(profileId : number) : Observable<Ipsum[]> {
+		return(this.http.get(this.ipsumUrl + profileId)
+			.map(BaseService.extractData)
+			.catch(BaseService.handleError));
+	}
+	getAllIpsum() : Observable<Ipsum[]> {
 		return(this.http.get(this.ipsumUrl)
 			.map(BaseService.extractData)
 			.catch(BaseService.handleError));

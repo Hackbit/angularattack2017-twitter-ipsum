7357c835e2cbbf36f39ca032e5c03bb42d650f03
Bradley Brown <tall.white.ninja@gmail.com>
2017-04-22 20:48:32 -0600

    updated ipsum request and service

---
+++ angular/classes/ipsum-request.ts
@@ -0,0 +1,3 @@
+export class IpsumRequest {
+	constructor(public twitterUserAtHandle: string) {}
+}
--- angular/services/ipsum-service.ts
+++ angular/services/ipsum-service.ts
@@ -2,10 +2,21 @@ import {Injectable} from "@angular/core";
 import {Http} from "@angular/http";
 import {BaseService} from "./base-service";
 import {Status} from "../classes/status";
-import {Ipsum} from "../classes/Ipsum";
+import {Ipsum} from "../classes/ipsum";
 import {Observable} from "rxjs/Observable";
 
 @Injectable ()
 export class IpsumService extends BaseService {
+	constructor(protected http:Http ) {
+		super(http);
+	}
 
+	//define the API endpoint
+	private ipsumUrl = "api/ipsum/";
+
+	getFeed() : Observable<Ipsum[]> {
+		return(this.http.get(this.ipsumUrl)
+			.map(BaseService.extractData)
+			.catch(BaseService.handleError));
+	}
 }

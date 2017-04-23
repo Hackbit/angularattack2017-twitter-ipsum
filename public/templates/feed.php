cf0a2e3ee8933d7120593e3bc772ad59657b2102
S JACKSON <jackson.sabastian@gmail.com>
2017-04-22 22:07:52 -0600

    right track - for sure :-) - feed.php update

--- public/templates/feed.php
+++ public/templates/feed.php
@@ -1,1 +1,50 @@
-<h1>FeedComponent</h1>
+<div class="row">
+	<div class="col-xs-8 col-xs-offset-2">
+		<div class="well well-lg ipsum-well">
+			<div class="pull-left">
+				<img src="../images/kitties49x49.png" alt="KITTIES!">
+			</div>
+			<div class="at-handle pull-left">
+				<p> @realDonaldTrump</p>
+			</div>
+			<button type="button" class="btn btn-default copy-button pull-right">Copy +</button>
+			<br/>
+			<br/>
+			<br/>
+			<p class="ipsum-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus non justo at mauris
+				tristique viverra sit amet et enim. Nulla ac scelerisque nisl. Pellentesque sagittis euismod ullamcorper.
+				Praesent ullamcorper tristique aliquam. Nullam semper sed urna et lobortis. Aliquam sed eros lorem.
+				Vestibulum sodales lectus vel quam molestie, ac tempor ante vehicula. Quisque elementum est turpis, vel
+				fringilla ipsum vehicula non. Orci varius natoque penatibus et magnis dis parturient montes, nascetur
+				ridiculus mus. Nulla facilisi.
+				<button type="button" class="btn btn-default copy-button pull-right">Share +</button>
+			</p>
+		</div>
+	</div>
+</div>
+<br>
+<div class="row">
+	<div class="col-xs-8 col-xs-offset-2">
+		<div class="well well-lg ipsum-well">
+			<div class="pull-left">
+				<img src="../images/kitties49x49.png" alt="KITTIES!">
+			</div>
+			<div class="at-handle pull-left">
+				<p> @realDonaldTrump</p>
+			</div>
+			<button type="button" class="btn btn-default copy-button pull-right">Copy +</button>
+			<br/>
+			<br/>
+			<br/>
+			<p class="ipsum-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus non justo at mauris
+				tristique viverra sit amet et enim. Nulla ac scelerisque nisl. Pellentesque sagittis euismod ullamcorper.
+				Praesent ullamcorper tristique aliquam. Nullam semper sed urna et lobortis. Aliquam sed eros lorem.
+				Vestibulum sodales lectus vel quam molestie, ac tempor ante vehicula. Quisque elementum est turpis, vel
+				fringilla ipsum vehicula non. Orci varius natoque penatibus et magnis dis parturient montes, nascetur
+				ridiculus mus. Nulla facilisi.
+				<button type="button" class="btn btn-default copy-button pull-right">Share +</button>
+			</p>
+		</div>
+	</div>
+</div>
+

28147b02ea016b36e3ca3625072d93d9792bb012
Dylan McDonald <dmcdonald21@cnm.edu>
2017-04-23 09:35:30 -0600

    API actually posts ipsum

--- routes/api.php
+++ routes/api.php
@@ -25,7 +25,26 @@ Route::post("/ipsum", function (Request $request) {
 		throw(new RuntimeException("You are not logged in. Please login with Twitter."));
 	}
 	$requestObject = $request->json();
-	$twitterUser = TwitterUser::where("twitterUserAtHandle", $requestObject->get("twitterUserAtHandle"))->first();
+	$tweetTwitterAtHandle = $requestObject->get("twitterUserAtHandle");
+
+	try {
+		$twitterUser = TwitterUser::where("TwitterUserAtHandle", $tweetTwitterAtHandle)->firstOrFail();
+	} catch(ModelNotFoundException $modelNotFoundException) {
+		$twitterUser = null;
+		$twitterData = json_decode(Twitter::getUserTimeline(["screen_name" => $tweetTwitterAtHandle, "format" => "json"]));
+		foreach($twitterData as $tweet) {
+			if($twitterUser === null) {
+				// create a fake database record for the finally block and then insert into mySQL
+				$twitterUser = new \stdClass();
+				$twitterUser->twitterUserId = $tweet->user->id_str;
+				TwitterUser::create(["twitterUserId" => $tweet->user->id_str, "twitterUserAtHandle" => $tweet->user->screen_name, "twitterUserImage" => $tweet->user->profile_image_url_https]);
+			}
+
+			$linkedTweetContent = Twitter::linkify($tweet);
+			Tweet::create(["tweetId" => $tweet->id_str, "tweetTwitterUserId" => $tweet->user->id_str, "tweetContent" => $linkedTweetContent]);
+		}
+	}
+
 	$tweets = Tweet::where("tweetTwitterUserId", $twitterUser->twitterUserId)->get();
 	$tweetToShuffle = "";
 	foreach($tweets as $tweetObject) {
@@ -35,7 +54,7 @@ Route::post("/ipsum", function (Request $request) {
 	$tweetToShuffle = explode(" ", $tweetToShuffle);
 	shuffle($tweetToShuffle);
 	$ipsum = implode(" ", $tweetToShuffle);
-	Ipsum::create(["ipsumProfileId" => Session::get("profileId"), "ipsumTwitterUserId" => $twitterUser->twitterUserId, "ipsumContent" => $ipsum]);
+	Ipsum::create(["ipsumProfileId" => $request->session()->get("profile")->profileId, "ipsumTwitterUserId" => $twitterUser->twitterUserId, "ipsumContent" => $ipsum]);
 });
 
 Route::get('/ipsum', function () {
@@ -67,28 +86,6 @@ Route::post("/profile", function (Request $request) {
 
 Route::get("profile/{id}", function ($id) {
 	return Profile::where("profileId", $id)->get();
-});
-
-Route::get("/tweetByTweetTwitterAtHandle/{tweetTwitterAtHandle}", function ($tweetTwitterAtHandle) {
-	try {
-		$twitterUser = TwitterUser::where("TwitterUserAtHandle", $tweetTwitterAtHandle)->firstOrFail();
-	} catch(ModelNotFoundException $modelNotFoundException) {
-		$twitterUser = null;
-		$twitterData = json_decode(Twitter::getUserTimeline(["screen_name" => $tweetTwitterAtHandle, "format" => "json"]));
-		foreach($twitterData as $tweet) {
-			if($twitterUser === null) {
-				// create a fake database record for the finally block and then insert into mySQL
-				$twitterUser = new \stdClass();
-				$twitterUser->twitterUserId = $tweet->user->id_str;
-				TwitterUser::create(["twitterUserId" => $tweet->user->id_str, "twitterUserAtHandle" => $tweet->user->screen_name, "twitterUserImage" => $tweet->user->profile_image_url_https]);
-			}
-
-			$linkedTweetContent = Twitter::linkify($tweet);
-			Tweet::create(["tweetId" => $tweet->id_str, "tweetTwitterUserId" => $tweet->user->id_str, "tweetContent" => $linkedTweetContent]);
-		}
-	} finally {
-		return (Tweet::where("tweetTwitterUserId", $twitterUser->twitterUserId)->get());
-	}
 });
 
 Route::get("/tweet/{tweetTwitterUserId}", function ($twitterUserId) {

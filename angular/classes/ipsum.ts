99440e72f281cd54d8d3c5421ef0348043bfc9e1
Bradley Brown <tall.white.ninja@gmail.com>
2017-04-22 20:37:30 -0600

    added ipsum service and class

---
+++ angular/classes/ipsum.ts
@@ -0,0 +1,3 @@
+export class Ipsum {
+	constructor(public ipsumId: number, public ipsumProfileId: number, public ipsumContent: string, public ipsumDateTime: Date) {}
+}
---
+++ angular/services/ipsum-service.ts
@@ -0,0 +1,11 @@
+import {Injectable} from "@angular/core";
+import {Http} from "@angular/http";
+import {BaseService} from "./base-service";
+import {Status} from "../classes/status";
+import {Ipsum} from "../classes/Ipsum";
+import {Observable} from "rxjs/Observable";
+
+@Injectable ()
+export class IpsumService extends BaseService {
+
+}

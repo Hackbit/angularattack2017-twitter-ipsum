8ccbe20ce681e4c58802870f4e87ac9654268652
Dylan McDonald <dmcdonald21@cnm.edu>
2017-04-23 08:01:17 -0600

    added ipsum create directive

---
+++ angular/components/ipsum-create-component.ts
@@ -0,0 +1,11 @@
+import {Component, Input} from "@angular/core";
+import {Ipsum} from "../classes/ipsum";
+import {TwitterUser} from "../classes/twitter-user";
+
+@Component({
+	selector: "ipsum-create",
+	templateUrl: "./templates/ipsum-create.php"
+})
+
+export class IpsumCreateComponent {
+}
---
+++ public/templates/ipsum-create.php
@@ -0,0 +1,12 @@
+<h2>Ipsum Create</h2>
+<div class="row">
+	<div class="col-xs-8 col-xs-offset-2">
+		<div class="input-group main-search">
+			<span class="input-group-addon" id="at-add-on">@</span>
+			<input type="text" class="form-control" placeholder="realDonaldTrump">
+			<span class="input-group-btn">
+				<button class="btn btn-default" type="button">Generate!</button>
+			</span>
+		</div>
+	</div>
+</div>

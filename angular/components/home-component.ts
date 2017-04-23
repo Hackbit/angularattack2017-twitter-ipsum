10f982326d99478f381fbb8c012d72b99c679086
Bradley Brown <tall.white.ninja@gmail.com>
2017-04-23 11:17:02 -0600

    fixing homepage that I broke...

--- angular/components/home-component.ts
+++ angular/components/home-component.ts
@@ -7,17 +7,5 @@ import {TwitterUser} from "../classes/twitter-user";
 })
 
 export class HomeComponent {
-	userNames : any[] = [];
 
-	searchString : string;
-
-	constructor(private autocompleteService: AutocompleteService) {}
-
-	ngOnInit() : void {
-		this.getAutoCompleteNames();
-	}
-
-	getAutoCompleteNames() : void {
-		//this.autocompleteService.fetchUserNames(this.searchString).subscribe(userNames => this.userNames = userNames);
-	}
 }
